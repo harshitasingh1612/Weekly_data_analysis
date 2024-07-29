d1adbb9782529cdc3b46509f99e00eb997d6c214
Harshita Singh <harshitasingh1612@gmail.com>
2024-07-28 20:47:39 -0700

    final code

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ def main():
     dataProcessObj = data_processing.dataProcessing()
     metrics_df = dataProcessObj.read_csv_from_filepath('S&A - Written Project - Data Set - raw_data.csv')
     dataProcessObj.transform_data(metrics_df)
-    Decomposed_effects_by_country = dataProcessObj.decompose_and_calculate_effects_by_country(metrics_df)
-    Decomposed_effects_by_country_browser = dataProcessObj.decompose_and_calculate_effects_by_country_browser(metrics_df)
-    Decomposed_effects_by_dimension = dataProcessObj.decompose_and_calculate_effects_by_dimension(metrics_df, ['country', 'browser'])
-    dataProcessObj.analyze_decomposition(Decomposed_effects_by_dimension)
-    dataProcessObj.weekly_analysis(metrics_df)
+    
+    # Decomposed_effects_by_country = dataProcessObj.decompose_and_calculate_effects_by_country(metrics_df)
+    # Decomposed_effects_by_country_browser = dataProcessObj.decompose_and_calculate_effects_by_country_browser(metrics_df)
+    # Decomposed_effects_by_dimension = dataProcessObj.decompose_and_calculate_effects_by_dimension(metrics_df, ['country', 'browser'])
+    # print(Decomposed_effects_by_dimension)
+    # dataProcessObj.analyze_decomposition(Decomposed_effects_by_country)
+    #df = Decomposed_effects_by_dimension[(Decomposed_effects_by_dimension['country'] == 'AJ') & (Decomposed_effects_by_dimension['week_before'] == 6) & (Decomposed_effects_by_dimension['week_after'] == 7)]
+    #print(df)
+    # dataProcessObj.calculate_weekly_country_increase(metrics_df, 7)
+    dataProcessObj.plot_metrics_per_country(metrics_df)
+    # dataProcessObj.calculate_weekly_browser_increase(metrics_df, 7)
+    # dataProcessObj.calculate_weekly_country_browser_increase(metrics_df, 7)
+    # dataProcessObj.calculate_weekly_country_visits_increase(metrics_df, 7)
+    # dataProcessObj.calculate_weekly_browser_visits_increase(metrics_df, 7)
+    # dataProcessObj.calculate_weekly_country_browser_visits_increase(metrics_df, 7)
+    # df = dataProcessObj.calculate_aggregates_by_country(metrics_df)
+    # dataProcessObj.week_country_analysis(df)
+
 if __name__ == "__main__":
     main()
--- src/README.md
+++ src/README.md
@@ -2,36 +2,33 @@
 1. Bar Chart
 
 2. Is there anything interesting about the trend?
-The conversion rate increased drastically from week 6(.35) to week 7(0.40)
+The global conversion rate increased by 9.79% from week 6 to week 7 across all countries and browsers.
+Overall the global conversion rate increased by 4.56% from week 0 to week 19 across all countries and browsers.
 
 3. How can you explain your finding(s) from the aggregated longitudinal data?
-In country AC, the number of visits and conversions increased by 124% across all the browsers. Due to this the overall conversion rate increased from week6 % to week 7 %
+
 
 4. What is your hypothesis on why the global conversion rate increased?
 
     - Qualitative Hypotheses
-        * Effective Marketing Campaigns:
-        * Improved User Experience:
-        * Product Enhancements:
-
-            1. Introduction of a popular product feature or service.
-            2. Better product availability or variety.
+        * Effective Marketing Campaigns
+        * Improved User Experience
+            1. Localization of the language
         * Positive External Influence:
-
             1. Favorable media coverage or reviews.
             2. Cultural or social trends aligning with offerings.
 
     - Quantitative Hypotheses
         * Increased Traffic:
-
             1. Surge in visitors due to advertising or SEO improvements.
             2. Higher engagement from specific demographics or regions.
-        * Higher Conversion Funnel Efficiency:
 
+        * Higher Conversion Funnel Efficiency:
             1. Improved click-through rates (CTR) from ads to landing pages.
-            2. Reduction in cart abandonment rates.
+    
         * Seasonal Demand:
             1. Sales spikes due to holidays or events.
+
         * Pricing Strategy:
             1. Discounts or promotions driving conversions.
             2. Competitive pricing adjustments.
@@ -51,3 +48,6 @@ The country - browser combination AC - Chrome is the top proportion change contr
 4. Written test function
 
 
+
+
+#AH-chrome, AJ-ie,AH-ie
